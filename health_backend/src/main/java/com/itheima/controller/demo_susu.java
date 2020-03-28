c53dc0dcd045ee43b7b9975cf71322c279f96a6f
root <root>
2020-03-28 16:15:35 +0800

    2020.03.28苏胜超完成

---
+++ health_backend/src/main/java/com/itheima/controller/demo_susu.java
@@ -0,0 +1,15 @@
+package com.itheima.controller;
+
+/**
+ * @PackageName: com.itheima.controller
+ * @ClassName: demo_susu
+ * @Author: sushengchao
+ * @Date: 2020/3/28 16:11
+ * @Description:
+ */
+public class demo_susu {
+
+    public static void main(String[] args) {
+        System.out.println("我叫苏胜超");
+    }
+}
